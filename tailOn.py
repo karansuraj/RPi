fce6e1b5f30858cf774ccdcb2478e1b464ad817d
Karan Suraj <karansuraj@gmail.com>
2018-05-24 10:18:47 -0400

    Create tailOn.py

---
+++ tailOn.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO, time, signal, sys, os
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+i = 1
+
+try:
+    while(1):
+        GPIO.output(17,True)
+        '''time.sleep(i)
+        GPIO.output(17, False)
+        time.sleep(i)'''
+except KeyboardInterrupt:
+    print('Program Interrupted')
+    try:
+        #GPIO.output(17,False)
+        GPIO.cleanup()
+    except SystemExit:
+        os._exit(0)
+
+
